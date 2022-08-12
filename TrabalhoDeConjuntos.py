4e583e8e1dd3a48519f1a3e85ceb79362723b8ef
Felipe de Lima dos Santos <87097189+yFells@users.noreply.github.com>
2022-08-12 14:19:04 -0300

    NewCommit
    Tirado Comentários

--- TrabalhoDeConjuntos.py
+++ TrabalhoDeConjuntos.py
@@ -1,13 +1,7 @@
 f = open("trabalhos.txt", 'r')
 conjuntos = f.readlines()
 
-
 qtd = int(conjuntos[0])
-"""
-int(input("Quantas operações você irá fazer? \n"))
-"""
-
-x = 0
 
 def printar(x,y,z):
     for c in range(len(y)):
@@ -64,25 +58,6 @@ def cart(linhas,colunas):
         for c in lista2:
             print("(" + i + c + ")", end=",")
 
-    """ CRIADO MATRIZ Item x Item
-    mat = []
-    matriz = []
-    for i in range(0, len(colunas)):
-        for c in range(0, len(linhas)):
-            mat.append([linhas[i],colunas[c]])
-    print("O resultado de seus PONTOS CARTESIANOS é ")
-    for i in range(0, len(mat)):
-        if i % len(colunas) == 0 and i != 0:
-            print("\n")
-            print(mat[i],end = " ")
-        else:
-            print(mat[i],end =" ")
-"""
-
-""" COMEÇO """
-
-
-
 for i in range(1, len(conjuntos), 3):
     oper = conjuntos[i]
     valores1 = conjuntos[i+1]
@@ -99,27 +74,3 @@ for i in range(1, len(conjuntos), 3):
         difer(val1, val2)
     elif operacao == "C":
         cart(val1, val2)
-
-
-
-
-"""  
-    oper = input("Qual operação você deseja fazer? \n união (U), uma interseção (I), um diferença (D) e um produto cartesiano (C) \n").upper()
-
-    valores1 = input("Qual os valores do primeiro conjunto? \n").replace(" ",",")
-    valores2 = input("Qual os valores do segundo conjunto? \n").replace(" ",",")
-
-    val1 = valores1.split(",")
-    val2 = valores2.split(",")
-
-    if oper == "U":
-        uniao(val1,val2)
-    elif oper == "I":
-        inter(val1,val2)
-    elif oper == "D":
-        difer(val1,val2)
-    elif oper == "C":
-        cart(val1,val2)
-
-    x = x + 1
-"""
