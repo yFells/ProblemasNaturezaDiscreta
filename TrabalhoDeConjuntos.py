830f68e802d7cb76fa8958b4b2969a9577b0ee6f
Felipe de Lima dos Santos <87097189+yFells@users.noreply.github.com>
2022-08-12 11:29:09 -0300

    NewCommit
    Adicionado a Leitura TXT
    Reformulado a função CART

--- TrabalhoDeConjuntos.py
+++ TrabalhoDeConjuntos.py
@@ -1,4 +1,12 @@
-qtd = int(input("Quantas operações você irá fazer? \n"))
+f = open("trabalhos.txt", 'r')
+conjuntos = f.readlines()
+
+
+qtd = int(conjuntos[0])
+"""
+int(input("Quantas operações você irá fazer? \n"))
+"""
+
 x = 0
 
 def printar(x,y,z):
@@ -6,23 +14,25 @@ def printar(x,y,z):
         print(y[c])
 
 def uniao(value1, value2):
-
-    for i in value1:
-        for x in value2:
+    lista1 = value1.replace("\n","").replace(" ","").split(",")
+    lista2 = value2.replace("\n","").replace(" ","").split(",")
+    for i in lista1:
+        for x in lista2:
             if x == i:
-                value2.remove(x)
-    for i in range(len(value2)):
-        value1.append(value2[i])
+                lista2.remove(x)
+    for i in range(len(lista2)):
+        lista1.append(lista2[i])
 
-    value1
-    s = ",".join([str(i) for i in value1])
+    s = ",".join([str(i) for i in lista1])
     print("O resultado da sua UNIÃO é ")
     print("{"+s+"}")
 
 def inter(value1, value2):
+    lista1 = value1.replace("\n", "").replace(" ", "").split(",")
+    lista2 = value2.replace("\n", "").replace(" ", "").split(",")
     lista = []
-    for i in value1:
-        for x in value2:
+    for i in lista1:
+        for x in lista2:
             if i == x:
                 lista.append(i)
 
@@ -32,20 +42,29 @@ def inter(value1, value2):
 
 
 def difer(value1,value2):
+    lista1 = value1.replace("\n", "").replace(" ", "").split(",")
+    lista2 = value2.replace("\n", "").replace(" ", "").split(",")
     lista = []
-    for i in value1:
-        for x in value2:
+    for i in lista1:
+        for x in lista2:
             if i == x:
                 lista.append(i)
     for i in lista:
-        value1.remove(i)
+        lista1.remove(i)
 
-    s = ",".join([str(i) for i in value1])
+    s = ",".join([str(i) for i in lista1])
     print("O resultado da sua DIFERENÇA é ")
     print("{" + s + "}")
 
 def cart(linhas,colunas):
-    """ CRIADO MATRIZ Item x Item """
+    lista1 = linhas.replace("\n", "").replace(" ", "").split(",")
+    lista2 = colunas.replace("\n", "").replace(" ", "").split(",")
+    print("O Resultado dos seus PRODUTOS CARTESIANOS é ")
+    for i in lista1:
+        for c in lista2:
+            print("(" + i + c + ")", end=",")
+
+    """ CRIADO MATRIZ Item x Item
     mat = []
     matriz = []
     for i in range(0, len(colunas)):
@@ -58,11 +77,33 @@ def cart(linhas,colunas):
             print(mat[i],end = " ")
         else:
             print(mat[i],end =" ")
+"""
+
+""" COMEÇO """
+
+
+
+for i in range(1, len(conjuntos), 3):
+    oper = conjuntos[i]
+    valores1 = conjuntos[i+1]
+    valores2 = conjuntos[i+2]
+    operacao = oper.replace("\n","")
+    val1 = valores1
+    val2 = valores2
+
+    if operacao == "U":
+        uniao(val1, val2)
+    elif operacao == "I":
+        inter(val1, val2)
+    elif operacao == "D":
+        difer(val1, val2)
+    elif operacao == "C":
+        cart(val1, val2)
 
 
 
 
-while x < qtd:
+"""  
     oper = input("Qual operação você deseja fazer? \n união (U), uma interseção (I), um diferença (D) e um produto cartesiano (C) \n").upper()
 
     valores1 = input("Qual os valores do primeiro conjunto? \n").replace(" ",",")
@@ -81,3 +122,4 @@ while x < qtd:
         cart(val1,val2)
 
     x = x + 1
+"""
